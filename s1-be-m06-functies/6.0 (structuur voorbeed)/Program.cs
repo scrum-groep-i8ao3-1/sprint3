a2f79e9772ba84bf0b4f4d7f75cfd36f0a66a7f9
dannyoo7 <dannylanssink@outloo.com>
2018-12-11 10:37:21 +0100

    psuh

---
+++ s1-be-m06-functies/6.0 (structuur voorbeed)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._0__structuur_voorbeed_
+    // naam
+    //punten
+    //start datum
+    // controleur
+    // controle goed of fout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
