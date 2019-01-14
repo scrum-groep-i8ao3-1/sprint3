7b9f51083010775e6e8c525021eb8a015a020555
dannyoo7 <dannylanssink@outloo.com>
2019-01-14 09:06:03 +0100

    acceptatie test

--- s1-be-m06-functies/6.3/Program.cs
+++ s1-be-m06-functies/6.3/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _6._3
 {
+    //danny
+    //20
+    //niet gebeurd
+    //14-12-2018
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("geef een datum in als : d/mm/yyyy");
+            string input = Console.ReadLine();
+            string createddate = Convert.ToDateTime(input).ToString("MM");
+            CultureInfo MyCultureInfo = new CultureInfo("nl");
+            DateTime MyDateTime = DateTime.Parse(input, MyCultureInfo);
+            Console.WriteLine(MyDateTime);
+            Console.ReadLine();
+            // The example displays the following output:
+            //       6/12/2008 12:00:00 AM
+        }
+        public static void zodiac()
+        {
+
         }
     }
 }
