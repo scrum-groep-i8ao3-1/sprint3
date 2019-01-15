5849b71e75cd8ff0eb5d83f61a4ff3e64f2de13f
dannyoo7 <dannylanssink@outloo.com>
2019-01-15 13:16:07 +0100

    6.9 pre alva

---
+++ s1-be-m06-functies/6.9/6.9/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        //klok laten zien!
+        static public Array nummer(int input)
+        {
+            //array var maken
+            string[,] Onummer = new string[6,6];
+            if (input == 0){
+                for (int teller = 6; teller >= 0; teller--)
+                {
+                    Onummer[teller, 6] = "*";
+                    Onummer[6, teller] = "*";
+                }
+                for (int teller = 6; teller >= 0; teller--)
+                {
+                    Onummer[teller, 6] = "*";
+                    Onummer[6, teller] = "*";
+                }
+            }
+            else if (input == 1)
+            {
+                for (int teller = 6; teller >= 0; teller--)
+                {
+                    Onummer[teller, 3] = "*";
+                    Onummer[teller, 4] = "*";
+                }
+                for (int teller = 6; teller >= 3; teller--)
+                {
+                    Onummer[0, teller] = "*";
+                    Onummer[1, teller] = "*";
+                }
+            }
+            else if (input == 2)
+            {
+
+            }
+            else if (input == 3)
+            {
+
+            }
+            else if (input == 4)
+            {
+
+            }
+            else if (input == 5)
+            {
+
+            }
+            else if (input == 6)
+            {
+
+            }
+            else if (input == 7)
+            {
+
+            }
+            else if (input == 8)
+            {
+
+            }
+            else if (input == 9)
+            {
+
+            }
+            else
+            {
+
+            }
+            return Onummer;
+        }
+    }
+}
