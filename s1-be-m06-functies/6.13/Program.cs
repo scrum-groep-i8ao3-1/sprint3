3dd66201dc41d8c7be18048f7eed18dbc307a991
yaboySLAM <44971734+yaboySLAM@users.noreply.github.com>
2019-01-15 08:53:09 +0100

    eerste push van timur na de vakantie

---
+++ s1-be-m06-functies/6.13/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace _6._13
+
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Create new SoundPlayer in the using statement.
+            using (SoundPlayer player = new SoundPlayer("C:\\bass.wav"))
+            {
+                // Use PlaySync to load and then play the sound.
+                // ... The program will pause until the sound is complete.
+                player.PlaySync();
+            }
+        }
+    }
+}
---
+++ s1-be-m06-functies/6.15/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
