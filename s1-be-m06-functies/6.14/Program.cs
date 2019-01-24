679c13b26410c53ae955adb5bef69daaefaadd71
DangerZoneDaniel <44972040+DangerZoneDaniel@users.noreply.github.com>
2019-01-24 11:11:52 +0100

    fetus deletus

---
+++ s1-be-m06-functies/6.14/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Voer uw geboortedatum in. ");
+            string geboortedatum = Convert.ToString(Console.ReadLine());
+            DateTime datum = DateTime.Parse(geboortedatum);
+            BerekenLeeftijd(geboortedatum);
+        }
+        public static void BerekenLeeftijd(string geboortedatum)
+        {
+        }
+    }
+}
--- s1-be-m06-functies/6.2/Program.cs
+++ s1-be-m06-functies/6.2/Program.cs
@@ -11,6 +11,20 @@ namespace _6._2
     {
         static void Main(string[] args)
         {
+            IsWeekend();
+        }
+        public static void IsWeekend()
+        {
+            DayOfWeek today = DateTime.Today.DayOfWeek;
+            var culture = new System.Globalization.CultureInfo("de-DE");
+            var day = culture.DateTimeFormat.GetDayName(DateTime.Today.DayOfWeek);
+            if (today == DayOfWeek.Saturday || today == DayOfWeek.Sunday)
+            {
+                Console.WriteLine("Today it is: " + today + " so it's weekend.");
+            }
+            else
+                Console.WriteLine("Today it is: " + today + " so it's a workday.");
+            Console.ReadKey();
         }
     }
 }
