aef48a4e3515b01763ad2a7db0eb9faa6d50eaf3
DangerZoneDaniel <44972040+DangerZoneDaniel@users.noreply.github.com>
2019-02-01 12:17:49 +0100

    yodieyo

--- s1-be-m06-functies/6.14/Program.cs
+++ s1-be-m06-functies/6.14/Program.cs
@@ -11,12 +11,28 @@ namespace _6._14
         static void Main(string[] args)
         {
             Console.WriteLine("Voer uw geboortedatum in. ");
-            string geboortedatum = Convert.ToString(Console.ReadLine());
-            DateTime datum = DateTime.Parse(geboortedatum);
+            DateTime geboortedatum = Convert.ToDateTime(Console.ReadLine());
             BerekenLeeftijd(geboortedatum);
         }
-        public static void BerekenLeeftijd(string geboortedatum)
+        public static void BerekenLeeftijd(DateTime geboortedatum)
         {
+            int dag = geboortedatum.Day;
+            int maand = geboortedatum.Month;
+            DateTime today = DateTime.Today;
+            int age = today.Year - geboortedatum.Year; 
+            if (geboortedatum > today.AddYears(-age))
+            {
+                if (dag == 29 && maand == 2)
+                {
+                    age = age / 4;
+                }
+                else
+                {
+                    --age;
+                }
+            }
+            Console.WriteLine("Uw leeftijd: " + age);
+            Console.ReadLine();
         }
     }
 }
