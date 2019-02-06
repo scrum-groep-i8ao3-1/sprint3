021894b42db6f95bec9dc02a3b49192566eb02fa
DangerZoneDaniel <44972040+DangerZoneDaniel@users.noreply.github.com>
2019-02-06 10:23:46 +0100

    paar opdracchten

---
+++ s1-be-m07-arrays/7.1/7.3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] array = new int[25];
+            for (int i = 0; i < array.Length; i++)
+            {
+                int randomNumber = random.Next(0, 10001);
+                array[i] = randomNumber;
+                if (IsOdd(i))
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+                Console.ReadLine();
+        }
+        public static bool IsEven(int value)
+        {
+            return value % 2 == 0;
+        }
+        public static bool IsOdd(int value)
+        {
+            return value % 2 != 0;
+        }
+    }
+}  
