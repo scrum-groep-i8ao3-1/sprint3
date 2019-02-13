aeef412ff4578d4f53dcb596a1ed2a48701125df
Tom Morskieft <0311319@student.rocvantwente.nl>
2019-02-13 10:07:19 +0100

    'Tom'

---
+++ s1-be-m08-fouten/8.1/8.5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+
+
+namespace ConsoleApp1
+
+{
+
+    class Program
+
+    {
+        //Gemaakt door Tom
+        //Getest door Jairo
+
+        static Random r = new Random();
+
+        static void Main(string[] args)
+
+        {
+            //Een loop die 100 keer herhaald
+            for (int i = 0; i < 100; i++)
+
+            {
+                //kiest een random getal voor getal1
+                long getal1 = r.Next(int.MaxValue);
+
+                //kiest een random getal voor getal1
+                long getal2 = r.Next(int.MaxValue);
+
+                //Doet product1 keer product2 en laat de uitkomst zien
+                long product = getal1 * getal2;
+                Console.WriteLine(product);
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
