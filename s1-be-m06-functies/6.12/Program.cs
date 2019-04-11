59f01979ad7e872aa035eff5f37166d5f665af8f
jairokooistra <44971993+jairokooistra@users.noreply.github.com>
2019-04-11 16:55:37 +0200

    test

---
+++ s1-be-m06-functies/6.12/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    //jairo
+    class Program
+    {
+        public static void Main()
+        {
+            IsSchrikkelJaar();
+            IsSchrikkelJaar(10);
+            Console.ReadLine();
+        }
+
+
+        static void IsSchrikkelJaar()
+        {
+            //geeft aan of het huidige jaar een schrikkeljaar is
+            int jaar = Convert.ToInt32(DateTime.Now.Year);
+            bool waarheid = DateTime.IsLeapYear(jaar);
+            if (waarheid == true)
+            {
+                Console.WriteLine("dit is een schrikkeljaar");
+            }
+            else
+            {
+                Console.WriteLine("dit is geen schrikkeljaar");
+            }
+            Console.ReadLine();
+        }
+        static void IsSchrikkelJaar(int year)
+        {
+            bool b = true;
+
+            //geeft aan of een opgegeven jaar in een schrikkeljaar is
+            Console.WriteLine("geef het jaar");
+            year = Convert.ToInt32(Console.ReadLine());
+
+            b = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+            if (b)
+            {
+                Console.WriteLine(year + " het is een schrikkeljaar ");
+            }
+            else
+            {
+                Console.WriteLine(year + " het is geen schrikkeljaar ");
+            }
+            //geeft aan of het in de huidige eeuw is
+            if (year < 50)
+            {
+                Console.WriteLine("het jaartal ligt in de huidige eeuw");
+            }
+            else if (year >= 50)
+            {
+                Console.WriteLine("Het jaartal ligt niet in de huidige eeuw");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
--- s1-be-m06-functies/6.13/Program.cs
+++ s1-be-m06-functies/6.13/Program.cs
@@ -3,22 +3,165 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Media;
 
-namespace _6._13
-
+namespace ConsoleApp18
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            // Create new SoundPlayer in the using statement.
-            using (SoundPlayer player = new SoundPlayer("C:\\bass.wav"))
+            Console.WriteLine("geef een nummer");
+            int geluid = Convert.ToInt32(Console.ReadLine());
+            if (geluid == 1)
             {
-                // Use PlaySync to load and then play the sound.
-                // ... The program will pause until the sound is complete.
-                player.PlaySync();
+                //speelt het geluid af
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer("C:/Users/mylok/OneDrive/Documents/wolf.wav");
+                player.Play();
+                Console.WriteLine("Wolf");
+                Console.ReadLine();
             }
+            else if (geluid == 2)
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer("C:/Users/mylok/OneDrive/Documents/Turtle.wav");
+                player.Play();
+                Console.WriteLine("Turtle");
+                Console.ReadLine();
+            }
+            else if (geluid == 3)
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer("C:/Users/mylok/OneDrive/Documents/hyena.wav");
+                player.Play();
+                Console.WriteLine("Hyena");
+                Console.ReadLine();
+            }
+            else if (geluid == 4)
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer("C:/Users/mylok/OneDrive/Documents/jaguar.wav");
+                player.Play();
+                Console.WriteLine("Jaguar");
+                Console.ReadLine();
+            }
+            else if (geluid == 5)
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer("C:/Users/mylok/OneDrive/Documents/hog.wav");
+                player.Play();
+                Console.WriteLine("Hog");
+                Console.ReadLine();
+            }
+            else if (geluid == 6)
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer("C:/Users/mylok/OneDrive/Documents/baboon.wav");
+                player.Play();
+                Console.WriteLine("Baboon");
+                Console.ReadLine();
+            }
+            else if (geluid == 7)
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer("C:/Users/mylok/OneDrive/Documents/bark.wav");
+                player.Play();
+                Console.WriteLine("Bark");
+                Console.ReadLine();
+            }
+            else if (geluid == 8)
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer("C:/Users/mylok/OneDrive/Documents/meme.wav");
+                player.Play();
+                Console.WriteLine("Meme");
+                Console.ReadLine();
+            }
+
+            else if (geluid == 9)
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer("C:/Users/mylok/OneDrive/Documents/Vrouw.wav");
+                player.Play();
+                Console.WriteLine("Vrouw");
+                Console.ReadLine();
+            }
+            else if (geluid == 10)
+            {
+                Geluid();
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("dat is geen goed nummer");
+            }
+            Console.ReadKey();
+
         }
+        public static void Geluid()
+        {
+            //zorgt voor een random geluid
+            Random rnd = new Random();
+            int number = rnd.Next(1, 9);
+            int geluid = number;
+            if (geluid == 1)
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer("C:/Users/mylok/OneDrive/Documents/wolf.wav");
+                player.Play();
+                Console.WriteLine("Wolf");
+                Console.ReadLine();
+            }
+            else if (geluid == 2)
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer("C:/Users/mylok/OneDrive/Documents/Turtle.wav");
+                player.Play();
+                Console.WriteLine("Turtle");
+                Console.ReadLine();
+            }
+            else if (geluid == 3)
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer("C:/Users/mylok/OneDrive/Documents/hyena.wav");
+                player.Play();
+                Console.WriteLine("Hyena");
+                Console.ReadLine();
+            }
+            else if (geluid == 4)
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer("C:/Users/mylok/OneDrive/Documents/jaguar.wav");
+                player.Play();
+                Console.WriteLine("Jaguar");
+                Console.ReadLine();
+            }
+            else if (geluid == 5)
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer("C:/Users/mylok/OneDrive/Documents/hog.wav");
+                player.Play();
+                Console.WriteLine("Hog");
+                Console.ReadLine();
+            }
+            else if (geluid == 6)
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer("C:/Users/mylok/OneDrive/Documents/baboon.wav");
+                player.Play();
+                Console.WriteLine("Baboon");
+                Console.ReadLine();
+            }
+            else if (geluid == 7)
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer("C:/Users/mylok/OneDrive/Documents/bark.wav");
+                player.Play();
+                Console.WriteLine("Bark");
+                Console.ReadLine();
+            }
+            else if (geluid == 8)
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer("C:/Users/mylok/OneDrive/Documents/meme.wav");
+                player.Play();
+                Console.WriteLine("Meme");
+                Console.ReadLine();
+            }
+
+            else if (geluid == 9)
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer("C:/Users/mylok/OneDrive/Documents/Vrouw.wav");
+                player.Play();
+                Console.WriteLine("Vrouw");
+                Console.ReadLine();
+            }
+            Console.ReadKey();
+
+        }
+
     }
 }
